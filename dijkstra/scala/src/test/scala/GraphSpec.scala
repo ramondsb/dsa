15fe2bb8255199bf10052cf253df5af640eaac1b
Ramon Brito <git@ramondsb.com>
2023-04-06 22:25:20 +0100

    Fix typos

--- dijkstra/scala/src/test/scala/GraphSpec.scala
+++ dijkstra/scala/src/test/scala/GraphSpec.scala
@@ -4,11 +4,11 @@ import org.scalatest.matchers.should.Matchers
 class GraphSpec extends AnyFlatSpec with Matchers {
     val graph = AdjGraph(Seq(("a", 1, "b"), ("b", 2, "c")))
 
-    "AdjGraph" should "returns an ordered sequence of all vertices" in {
+    "AdjGraph" should "return an ordered sequence of all vertices" in {
         graph.vertices shouldBe Seq("a", "b", "c")
     }
     
-    it should "returns a sequence of its edges" in {
+    it should "return a sequence of its edges" in {
         graph.edges should contain theSameElementsAs Seq(("b", 2, "c"), ("a", 1, "b"))
     }
 
