8af68618b54e2a6165a4c455bf4fe5f23c3cc983
Ramon Brito <git@ramondsb.com>
2023-04-06 22:03:21 +0100

    Fix unit test description

--- dijkstra/scala/src/test/scala/GraphSpec.scala
+++ dijkstra/scala/src/test/scala/GraphSpec.scala
@@ -8,9 +8,10 @@ class GraphSpec extends AnyFlatSpec with Matchers {
         graph.vertices shouldBe Seq("a", "b", "c")
     }
     
-    it should "returns a ordered sequence of its edges" in {
+    it should "returns a sequence of its edges" in {
         graph.edges should contain theSameElementsAs Seq(("b", 2, "c"), ("a", 1, "b"))
     }
+
     // TODO: graph.vertice("a").neigbours shouldBe Seq["b"]
     // TODO: Add edge
     // TODO: Remove edge
